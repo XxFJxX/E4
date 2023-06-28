425e25b8011717852bf23abae39192e22f0e5221
XxFJxX <ferinocente357@gmail.com>
2023-06-28 00:50:41 -0600

    Agregar archivos de proyecto.

---
+++ E4/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace E4;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        int nume1 = Convert.ToInt32(num1.Text);
+
+		int r1 = nume1 * nume1;
+        int r2 = nume1 * nume1 * nume1;
+
+		la.Text = "El numero " + nume1 + " tiene de cuadrado " + r1 + " y el cubo " + r2;
+    }
+}
+
